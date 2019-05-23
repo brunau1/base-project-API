bdf3e79a070278217a3b66afd7d59861264e9627
brunau1 <brunosantosprotec@gmai.com>
2019-05-23 11:45:51 -0300

    structure changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("it works!")
+import express, {Request, Response} from 'express'
+
+const server = express()
+
+server.listen(3003, () => {
+    console.log("server listening on port 3003...")
+})
