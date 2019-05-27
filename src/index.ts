9ffcc0b85aa162091b8f44c31aa63e8aaccc1d5e
Bruno Santos <bruno.santos@kukac.com.br>
2019-05-27 11:09:16 -0300

    created the config script and applied to the server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express'
+import { config } from '../configurations/config'
 
 const server = express()
 
-server.listen(3003, () => {
-    console.log("server listening on port 3003...")
+server.listen(config.port, () => {
+    console.log("server listening on port"+config.port+"...")
 })
---
+++ src/models/ConfigInterface.ts
@@ -0,0 +1,6 @@
+export default interface ConfigInterface {
+    environment: string
+    host: string
+    port: string
+    endpoint: string
+}
